3df3a08ff647a85476755d552c0f8b1e63a9a23e
Adriics <adriancalvachesanchez@gmail.com>
2025-09-08 13:19:34 +0200

    feat: added code input

--- frontend/src/components/CodeInput.tsx
+++ frontend/src/components/CodeInput.tsx
@@ -1,1 +1,12 @@
-export function CodeInput() {}
+import TextareaAutosize from "@mui/material/TextareaAutosize"
+
+export default function CodeInput() {
+  return (
+    <TextareaAutosize
+      aria-label="minimum height"
+      minRows={20}
+      placeholder="Minimum 3 rows"
+      style={{ width: 500 }}
+    />
+  )
+}
